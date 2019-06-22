d6bb4361da8a156a34bc126a2883787f742f481e
Faheem Khan <faheem5948@gmail.com>
2019-06-22 11:45:12 -0400

    added sign up for route
    get post form data body as json

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -8,6 +8,16 @@ app = Flask(__name__)
 @app.route("/home/", methods=["GET"])
 def homePage():
     return render_template("index.html")
+
+
+@app.route("/signUp", methods=["POST"])
+def signUpForm():
+    if request.method == "POST":
+        body = request.form
+        print(body) # JSON {body: "fname":""...}
+        # this is where you add the user to the db
+        return jsonify({"body":body})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=False)
